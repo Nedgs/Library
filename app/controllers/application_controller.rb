741c5e855c9cf6781c91982f49776bfec7f7b545
Nedgs <nadergwasmi25@gmail.com>
2024-06-24 16:39:19 +0200

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+  
+    before_action :require_login
+  
+    private
+  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  
+    def require_login
+      unless current_user
+        redirect_to login_path, alert: "You must be logged in to access this section"
+      end
+    end
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-    # Routes for User
+    # Routes pour User
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
     get '/register', to: 'users#new'
     post '/register', to: 'users#create'
   
-    # Routes for Book
-    resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    # Routes pour Book
+    resources :books
   
-    # Routes for Borrow
+    # Routes pour Borrow
     resources :borrows, only: [:create, :destroy]
   
     root 'books#index'
