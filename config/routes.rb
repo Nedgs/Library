70ac85411b39ee8706a74a0dce6103c700882eac
Nedgs <nadergwasmi25@gmail.com>
2024-06-24 16:53:25 +0200

    kk

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-    # Routes pour User
+    # Routes for User
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
     get '/register', to: 'users#new'
     post '/register', to: 'users#create'
   
-    # Routes pour Book
+    # Routes for Book
     resources :books
   
-    # Routes pour Borrow
+    # Routes for Borrow
     resources :borrows, only: [:create, :destroy]
   
     root 'books#index'
