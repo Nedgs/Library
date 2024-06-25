37ad8431bb4b99330d7a1406faf2e0891150f1c7
Nedgs <nadergwasmi25@gmail.com>
2024-06-25 16:44:41 +0200

    add search

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,13 @@ class Book < ApplicationRecord
     has_many :users, through: :borrows
   
     validates :title, :author, :publish_year, presence: true
+  
+    def self.search(query)
+      if query.present?
+        where('title LIKE ? OR author LIKE ? OR publish_year LIKE ?', "%#{query}%", "%#{query}%", "%#{query}%")
+      else
+        all
+      end
+    end
 end
   
